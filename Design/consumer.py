8377c52a96125d53f68938185f07f71f8ad42653
MohammadHamza Asif <81828400+Mohammad0336@users.noreply.github.com>
2024-02-17 23:08:57 -0500

    Add files via upload

---
+++ Design/consumer.py
@@ -0,0 +1,22 @@
+from google.cloud import pubsub_v1
+import json
+
+# credentials_info = json.load(open('cred.json'))
+project_id = "lab1-413419"
+subscription_id = "smartMeterTopic-sub"
+
+subscriber = pubsub_v1.SubscriberClient()
+subscription_path = subscriber.subscription_path(project_id, subscription_id)
+
+def callback(message):
+    print(f"Received message: {message.data.decode('utf-8')}")
+    message.ack()
+
+streaming_pull_future = subscriber.subscribe(subscription_path, callback=callback)
+print(f"Listening for messages on {subscription_path}...\n")
+
+try:
+    streaming_pull_future.result()
+except KeyboardInterrupt:
+    streaming_pull_future.cancel()
+
---
+++ Design/smartMeterTest.py
@@ -0,0 +1,80 @@
+import argparse
+import json
+import logging
+import os
+
+import apache_beam as beam
+import tensorflow as tf
+from apache_beam.options.pipeline_options import PipelineOptions
+from apache_beam.options.pipeline_options import SetupOptions
+
+
+def singleton(cls):
+  instances = {}
+  def getinstance(*args, **kwargs):
+    if cls not in instances:
+      instances[cls] = cls(*args, **kwargs)
+    return instances[cls]
+  return getinstance
+
+# processing function
+class PredictDoFn(beam.DoFn):
+
+    def process(self, metric_json_val):
+
+        # Filter out all values containing None
+        for key in metric_json_val.keys():
+            if (metric_json_val[key]) is None:
+                return Null
+
+
+        # Read in values from json
+        temp = float(metric_json_val['temperature'])
+        pres = float(metric_json_val['pressure'])
+
+        # Convert to imperial units
+        pres_imp = pres / 6.895
+        temp_imp = temp * 1.8 + 32
+
+        # Create a new Result dict
+        result = {
+            'time': metric_json_val['time'],
+            'pressure-psi': pres_imp,
+            'temperature-fahrenheit': temp_imp,
+            'pressure-kpa': metric_json_val['pressure'],
+            'temperature-celsius': metric_json_val['temperature'],
+        }
+
+        # Return created dict to be made into json
+        return [result]
+
+
+            
+def run(argv=None):
+  parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+  # input args
+  parser.add_argument('--input', dest='input', required=True, help='Input file to process.')
+  parser.add_argument('--output', dest='output', required=True,help='Output file to write results to.')
+
+
+  # parse args from pipeline
+  known_args, pipeline_args = parser.parse_known_args(argv)
+  pipeline_options = PipelineOptions(pipeline_args)
+  pipeline_options.view_as(SetupOptions).save_main_session = True;
+  
+  
+  with beam.Pipeline(options=pipeline_options) as p:
+    
+    # read data from topic, convert dict to json
+    images= (p | "Read from Pub/Sub" >> beam.io.ReadFromPubSub(topic=known_args.input)| "toDict" >> beam.Map(lambda x: json.loads(x)));
+        
+    # convert data values, return converted json
+    converted = images | 'convert units' >> beam.ParDo(PredictDoFn())
+    
+    # write converted json to output topic
+    (converted | 'to byte' >> beam.Map(lambda x: json.dumps(x).encode('utf8')) |   'to Pub/sub' >> beam.io.WriteToPubSub(topic=known_args.output));
+        
+if __name__ == '__main__':
+  logging.getLogger().setLevel(logging.INFO)
+  run()
